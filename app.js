c8451cb0b65a5211743e43f386915d176e066e8c
Martina Jirešová <martinka.jiresova@gmail.com>
2020-02-02 18:29:08 +0100

    publishing

---
+++ app.js
@@ -0,0 +1,13 @@
+const heading = 'BASIC JS AND CSS PROJECTS';
+let i = 0;
+
+const typing = () => {
+  if(i < heading.length) {
+    document.querySelector('.heading').innerHTML += heading.charAt(i);
+    i++;
+
+    setTimeout(typing, 150);
+  }
+}
+
+typing();
